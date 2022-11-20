eb03a994754919920d359de78b2f37e0f9882dfe
Ekaterina <nnnkateee@gmail.com>
2022-11-20 10:25:37 +0300

    Add folders

--- Weather/Weather/Main/MainViewController.swift
+++ Weather/Weather/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Weather
 //
 //  Created by Ekaterina Nedelko on 20.11.22.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
